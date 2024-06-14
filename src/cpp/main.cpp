c4673f53f33f01c55eefb7382d17dd69f6d743fc
mellw0101 <melwivensson@gmail.com>
2024-06-14 08:39:56 +0200

    Changes to be committed: 	new file:   .clang-format 	new file:   .vscode/settings.json 	modified:   build/bin/AmakeCpp 	modified:   build/obj/main.o 	modified:   src/cpp/main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,7 +1,9 @@
-#include <Mlib/Math.h>
-#include <Mlib/FileSys.h>
 #include <Mlib/Args.h>
+#include <Mlib/FileSys.h>
 #include <Mlib/def.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 #define VERSION "0.1"
 #define NAME "AmakeCpp"
@@ -9,74 +11,326 @@
 using namespace std;
 using namespace Mlib;
 
-namespace AmakeCpp
-{
-    /// @name printC
-    /// @brief Print string with color
-    /// @param s string to print
-    /// @param color color code
+string const cwd         = FileSys::currentWorkingDir();
+string const projectName = cwd.substr(cwd.find_last_of("/") + 1);
+string const SRC_DIR     = cwd + "/src";
+string const INCLUDE_DIR = SRC_DIR + "/include";
+string const AS_DIR      = SRC_DIR + "/as";
+string const CPP_DIR     = SRC_DIR + "/cpp";
+string const C_DIR       = SRC_DIR + "/c";
+string const BUILD_DIR   = cwd + "/build";
+string const OBJ_DIR     = BUILD_DIR + "/obj";
+string const BIN_DIR     = BUILD_DIR + "/bin";
+
+namespace AmakeCpp {
+    namespace Options {
+        /// @enum Option
+        /// @brief
+        /// - Enum representing cli options
+        enum Option
+        {
+            H,
+            C,
+            V,
+            B,
+            CL,
+            I,
+            UNKNOWN
+        };
+
+        /// @name optionFromArg
+        /// @brief
+        /// - Convert string to Option
+        /// @param arg
+        /// - string to convert
+        /// @returns Option
+        /// @note
+        /// - Option is an @enum
+        Option
+        optionFromArg(const string& arg)
+        {
+            static const unordered_map<string, Option> optionMap = {
+                {     "--help",  H},
+                {"--configure",  C},
+                {  "--version",  V},
+                {    "--build",  B},
+                {    "--clean", CL},
+                {  "--install",  I}
+            };
+            auto const it = optionMap.find(arg);
+            if (it != optionMap.end())
+            {
+                return it->second;
+            }
+            return UNKNOWN;
+        }
+
+        /// @enum ConfigureOption
+        /// @brief
+        /// - Enum representing configure sub options
+        enum ConfigureOption
+        {
+            CLANG_FORMAT
+        };
+
+        /// @name configureOptionFromArg
+        /// @brief
+        /// - Convert string to ConfigureOption
+        /// @param arg
+        /// - string to convert
+        /// @returns ConfigureOption
+        /// @note
+        /// - ConfigureOption is an @enum
+        ConfigureOption
+        configureOptionFromArg(const string& arg)
+        {
+            static const unordered_map<string, ConfigureOption> configureOptionMap = {
+                {"--clang-format", CLANG_FORMAT}
+            };
+            auto const it = configureOptionMap.find(arg);
+            if (it != configureOptionMap.end())
+            {
+                return it->second;
+            }
+            return CLANG_FORMAT;
+        }
+    } // namespace Options
+    using namespace Options;
+    namespace Tools {
+        /// @name printC
+        /// @returns void
+        /// @brief Print string with color
+        /// @param s string to print
+        /// @param color color code
+        /// @note
+        /// - color codes available:
+        ///   - ESC_CODE_RED
+        ///   - ESC_CODE_GREEN
+        ///   - ESC_CODE_YELLOW
+        ///   - ESC_CODE_BLUE
+        ///   - ESC_CODE_MAGENTA
+        ///   - ESC_CODE_CYAN
+        ///   - ESC_CODE_WHITE
+        ///   - ESC_CODE_RESET
+        void
+        printC(string const& str, const char* color)
+        {
+            cout << NAME << " - [ " << color << str << ESC_CODE_RESET << " ]" << '\n';
+        }
+
+        /// @name createProjectDir
+        /// @brief
+        /// - Create project directory
+        /// @param subPath
+        /// - Sub path to create
+        /// @note
+        /// - Creates directory in current working directory ( cwd + subPath )
+        /// @returns void
+        void
+        createProjectDir(const string& path)
+        {
+            try
+            {
+                printC("Creating Dir -> " + path, ESC_CODE_GREEN);
+                FileSys::mkdir(path);
+            }
+            catch (exception const& e)
+            {
+                printC(e.what(), ESC_CODE_RED);
+            }
+        }
+
+        /// @name compileCpp
+        /// @brief
+        /// - Compile ( .cpp -> .o ) in src/cpp
+        /// @returns void
+        void
+        compileCpp()
+        {
+            printC("Compiling .cpp -> .o", ESC_CODE_GREEN);
+            vector<string> files;
+            try
+            {
+                files = FileSys::dirContentToStrVec(cwd + "/src/cpp");
+                for (auto const& file : files)
+                {
+                    string fileName = file.substr(file.find_last_of("/") + 1);
+                    string objName  = cwd + "/build/obj/" + fileName.substr(0, fileName.find_last_of(".")) + ".o";
+                    printC(file + " -> " + objName, ESC_CODE_BLUE);
+                    system(("clang++ -O3 -march=native -c " + file + " -o " + objName).c_str());
+                }
+            }
+            catch (exception const& e)
+            {
+                printC(e.what(), ESC_CODE_RED);
+            }
+        }
+
+        /// @name linkBinary
+        /// @brief
+        /// - Link .o files in build/obj to binary in build/bin
+        /// @returns void
+        void
+        linkBinary()
+        {
+            printC("Linking Obj Files -> " + cwd + "/build/bin/" + projectName, ESC_CODE_GREEN);
+            vector<string> objVec = FileSys::dirContentToStrVec(cwd + "/build/obj");
+            string         objStr = Args::strFromStrVec(objVec);
+            system(("clang++ -O3 -march=native " + objStr + " -o " + cwd + "/build/bin/" + projectName).c_str());
+        }
+
+        void
+        configureProjectDirs()
+        {
+            createProjectDir(SRC_DIR);
+            createProjectDir(INCLUDE_DIR);
+            createProjectDir(AS_DIR);
+            createProjectDir(CPP_DIR);
+            createProjectDir(C_DIR);
+            createProjectDir(BUILD_DIR);
+            createProjectDir(OBJ_DIR);
+            createProjectDir(BIN_DIR);
+        }
+
+        void
+        configureClangFormat()
+        {
+            cout << "Configuring .clang-format\n";
+        }
+    } // namespace Tools
+    using namespace Tools;
+
+    /// @name Help
+    /// @brief
+    /// - Show help message
+    /// @returns void
     void
-    printC(string const &s, const char *color)
+    Help()
     {
-        cout << NAME << " - [ " << color  << s << ESC_CODE_RESET << " ]" << '\n';
+        cout << "Usage: " << NAME << " [options]\n"
+             << "Options:\n"
+             << "   --help                      Show this help message\n"
+             << "   --version                   Show version information\n"
+             << "   --configure                 Configure current dir as project\n"
+             << "   configure options:\n"
+             << "       none                    (default) Create Project in current directory\n"
+             << "       --clang-format          Configure .clang-format file for project\n"
+             << "   --build                     Build project\n"
+             << "   --clean                     Clean project\n"
+             << "   --install                   Install project\n";
     }
 
-    /// Print Help message
-	void
-    Help()
-	{
-		cout
-            << "Usage: " << NAME << " [options]\n"
-            << "Options:\n"
-            << "    -h,     --help          Show this help message\n"
-            << "    -v,     --version       Show version information\n"
-            << "    -c,     --configure     Configure current dir as project" << endl;
-	}
-
-	void
-    Configure()
-	{
-		string const cwd = FileSys::currentWorkingDir();
-		try
+    /// @name Configure
+    /// @brief
+    /// - Configure current directory as project
+    /// @param subOptions
+    /// - Sub options for configure
+    /// @returns void
+    void
+    Configure(const string& subOption = "")
+    {
+        if (subOption.empty())
         {
-            printC("Creating Dir -> " + cwd + "/src", ESC_CODE_GREEN);
-            FileSys::mkdir(cwd + "/src");
-            printC("Creating Dir -> " + cwd + "/include", ESC_CODE_GREEN);
-            FileSys::mkdir(cwd + "/include");
-            printC("Creating Dir -> " + cwd + "/build", ESC_CODE_GREEN);
-            FileSys::mkdir(cwd + "/build");
-            printC("Creating Dir -> " + cwd + "/bin", ESC_CODE_GREEN);
-            FileSys::mkdir(cwd + "/bin");
+            configureProjectDirs();
+        }
+        else
+        {
+            ConfigureOption const option = configureOptionFromArg(subOption);
+            switch (option)
+            {
+                case CLANG_FORMAT :
+                    try
+                    {
+                        configureClangFormat();
+                    }
+                    catch (exception const& e)
+                    {
+                        printC(e.what(), ESC_CODE_RED);
+                    }
+                    break;
+            }
+        }
+    }
+
+    /// @name Build
+    /// @brief
+    /// - Build project
+    void
+    Build()
+    {
+        compileCpp();
+    }
+
+    /// @name Clean
+    /// @brief
+    /// - Clean project
+    /// @returns void
+    void
+    Clean()
+    {
+        try
+        {
+            printC("Cleaning project", ESC_CODE_GREEN);
+            FileSys::rmdir(cwd + "/build");
         }
         catch (exception const& e)
         {
             printC(e.what(), ESC_CODE_RED);
-        }    
-	}
-}
+        }
+    }
+
+    /// @name Install
+    /// @brief
+    /// - Install project
+    /// @returns void
+    void
+    Install()
+    {
+        linkBinary();
+    }
+} // namespace AmakeCpp
+using namespace AmakeCpp;
 
 int
 main(int argc, char** argv)
 {
-	auto sArgv = Args::argvToStrVec(argc, argv);
-	for (int i=1; i<sArgv.size(); ++i)
-	{
-		if (sArgv[i] == "--help")
-		{
-			AmakeCpp::Help();
-		}
-		else if (sArgv[i] == "--version")
-		{
-			cout << "Version: " << VERSION << endl;
-		}
-		else if (sArgv[i] == "--configure")
-		{
-			AmakeCpp::Configure();
-		}
-		else
-		{
-			cerr << "Error: Unknown option " << sArgv[i] << '\n';
-		}
-	}
-	return 0;
+    auto sArgv = Args::argvToStrVec(argc, argv);
+    for (int i = 1; i < sArgv.size(); ++i)
+    {
+        Option const option = optionFromArg(sArgv[i]);
+        switch (option)
+        {
+            case H :
+                Help();
+                break;
+            case C :
+                if (i + 1 < sArgv.size())
+                {
+                    if (optionFromArg(sArgv[i + 1]) == UNKNOWN)
+                    {
+                        Configure(sArgv[i + 1]);
+                        ++i;
+                        break;
+                    }
+                }
+                Configure();
+                break;
+            case V :
+                cout << "Version: " << VERSION << '\n';
+                break;
+            case B :
+                Build();
+                break;
+            case CL :
+                Clean();
+                break;
+            case I :
+                Install();
+                break;
+            case UNKNOWN :
+                cerr << "Error: Unknown option '" << sArgv[i] << "'\n";
+                break;
+        }
+    }
+    return 0;
 }
