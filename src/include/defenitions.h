0ff52910883ddbf1737c122253cd267188619e3c
mellw0101 <melwivensson@gmail.com>
2024-12-07 01:25:20 +0000

    Changes to be committed: 	modified:   build/obj/main.o 	modified:   src/include/defenitions.h

--- src/include/defenitions.h
+++ src/include/defenitions.h
@@ -39,13 +39,14 @@
 
 #define ARG_MAX 256
 #define ASM_COMPILER "nasm"
-#define ASM_DEFAULT_ARGS "-f elf64"
 #define C_COMPILER  "clang"
 #define CC_COMPILER "clang++"
 #if defined(__x86_64__)
+  #define ASM_DEFAULT_ARGS "-f elf64"
   #define C_DEFAULT_ARGS "-m64 -funroll-loops -O3 -static -Werror -Wall -march=native -Rpass=loop-vectorize -flto -Wno-vla"
   #define CC_DEFAULT_ARGS "-m64 -stdlib=libc++ -funroll-loops -O3 -std=c++23 -static -Werror -Wall -march=native -Rpass=loop-vectorize -flto -Wno-vla -mavx"
 #elif defined(__aarch64__)
+  #define ASM_DEFAULT_ARGS ""
   #define C_DEFAULT_ARGS "-m64 -funroll-loops -O3 -static -Werror -Wall -Rpass=loop-vectorize -flto -Wno-vla"
   #define CC_DEFAULT_ARGS "-m64 -stdlib=libc++ -funroll-loops -O3 -std=c++23 -static -Werror -Wall -Rpass=loop-vectorize -flto -Wno-vla"
 #endif
